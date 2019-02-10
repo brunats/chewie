c5a598997d744e55d9446de8d6282ed2c19d1751
brunats <silvatavares.bruna@gmail.com>
2019-02-10 20:06:16 -0200

    Remove table from order form

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -5,19 +5,25 @@ class OrdersController < ApplicationController
   def index; end
 
   def new
-    @order = Order.new
+    @new_order = Order.new
   end
 
   def create
-    order = build_order
-    return redirect_to(root_path) if order.valid? && order.save
-    flash[:warning] = 'faile'
+    return redirect_to(root_path) if new_order.valid? && new_order.save
+    flash[:warning] = new_roder.errors.to_a.first
+
     render :new
   end
 
   def edit; end
 
-  def update; end
+  def update
+    order.customer = customer
+    return redirect_to(root_path) if order.valid? && order.save
+    flash[:warning] = order.errors.to_a.first
+
+    render :edit
+  end
 
   def destroy
     flash[:success] = I18n.t('.destroyed') if order.destroy
@@ -30,33 +36,15 @@ class OrdersController < ApplicationController
     @order ||= Order.find(params[:id])
   end
 
+  def new_order
+    @new_order ||= Order.new(customer: customer)
+  end
+
   def orders
     @orders ||= Order.all.includes(:customer, :order_products)
   end
 
   def customer
     @customer ||= Customer.find(params[:order][:customer_id])
-  end
-
-  def build_order_products
-    order_product_list = []
-    params[:unit_price].each do |id, unit_price|
-      units = params[:units][id]
-      next if units.to_i.zero? || unit_price.to_f.zero?
-      product = Product.find(id)
-      order_product_list << OrderProduct.new(
-        product: product,
-        unit_price: unit_price,
-        units: units
-      )
-    end
-    order_product_list
-  end
-
-  def build_order
-    Order.new(
-      customer: customer,
-      order_products: build_order_products
-    )
   end
 end
--- app/models/order_product.rb
+++ app/models/order_product.rb
@@ -1,6 +1,6 @@
 class OrderProduct < ApplicationRecord
-  validates :units, presence: true
-  validates :unit_price, presence: true
+  validates :units, presence: true, numericality: { greater_than: 0 }
+  validates :unit_price, presence: true, numericality: { greater_than: 0 }
   belongs_to :order
   belongs_to :product
 end
