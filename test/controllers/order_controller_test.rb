622ec1f6dc7a9873e7727fcd92c75dd888c028a9
brunats <silvatavares.bruna@gmail.com>
2019-02-10 01:51:25 -0200

    Testing using routes

--- test/controllers/order_controller_test.rb
+++ test/controllers/order_controller_test.rb
@@ -2,6 +2,7 @@ require 'test_helper'
 
 class OrderControllerTest < ActionDispatch::IntegrationTest
   include Rails.application.routes.url_helpers
+
   test 'index response success' do
     get root_path
     assert_response :success
