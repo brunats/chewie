126b4fd6592f0ab0cb95fdb4ae902b786efbe0e0
brunats <silvatavares.bruna@gmail.com>
2019-02-09 23:43:18 -0200

    Test order product mode

---
+++ test/models/order_product_test.rb
@@ -0,0 +1,33 @@
+require 'test_helper'
+
+class OrderProductTest < ActiveSupport::TestCase
+  setup do
+    @customer = Customer.create!(name: 'Chewie')
+    @order = Order.create!(customer: @customer)
+    @product = Product.create!(
+      name: 'Lightsaber',
+      price: 15_000,
+      multiple: 1
+    )
+
+    @order_product = OrderProduct.new(units: 2, unit_price: 4.5, order: @order, product: @product)
+  end
+
+  test 'belongs to order' do
+    @order_product.order = nil
+    assert_not @order_product.save
+    assert_equal @order_product.errors[:order], ['must exist']
+
+    @order_product.order = @order
+    assert @order_product.save
+  end
+
+  test 'belongs to product' do
+    @order_product.product = nil
+    assert_not @order_product.save
+    assert_equal @order_product.errors[:product], ['must exist']
+
+    @order_product.product = @product
+    assert @order_product.save
+  end
+end
