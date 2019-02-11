82c38bfdf758e467e3cb610167a26e0e0cd28975
brunats <silvatavares.bruna@gmail.com>
2019-02-11 21:05:34 -0200

    testing order controller

--- app/validators/multiple_unit_validator.rb
+++ app/validators/multiple_unit_validator.rb
@@ -1,14 +1,15 @@
 class MultipleUnitValidator < ActiveModel::Validator
   def validate(order_product)
-    return if order_product.product.blank? || multiple_unit(order_product).zero?
-    unless (order_product.units % multiple_unit).zero?
-      order_product.errors[:base] << I18n.t('.errors.multiple_unit_error', multiple_value: multiple_unit)
+    @order_product = order_product
+    return if @order_product.product.blank? || multiple_unit.zero?
+    unless (@order_product.units % multiple_unit).zero?
+      @order_product.errors[:base] << I18n.t('.errors.multiple_unit_error', multiple_value: multiple_unit)
     end
   end
 
   private
 
-  def multiple_unit(order_product = nil)
-    @multiple_unit ||= order_product.product.multiple
+  def multiple_unit
+    @order_product.product.multiple
   end
 end
--- test/controllers/order_controller_test.rb
+++ test/controllers/order_controller_test.rb
@@ -8,6 +8,11 @@ class OrderControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
+  test 'new order response success' do
+    get new_order_path
+    assert_response :success
+  end
+
   test 'delete an order' do
     order = Order.new()
     customer = Customer.create!(name: 'Chewie')
@@ -18,5 +23,40 @@ class OrderControllerTest < ActionDispatch::IntegrationTest
 
     assert_response :redirect
     assert Order.all.count.zero?
+  end
+
+  test 'create an order' do
+    assert Order.all.count.zero?
+
+    customer = Customer.create!(name: 'Chewie')
+    post orders_path, params: { order: { customer_id: customer.id } }
+
+    assert_response :redirect
+    assert_equal Order.all.count , 1
+  end
+
+  test 'edit an order' do
+    order = Order.new()
+    customer = Customer.create!(name: 'Chewie')
+    order.customer = customer
+    order.save!
+
+    get edit_order_path(order)
+
+    assert_response :success
+  end
+
+  test 'update an order' do
+    order = Order.new()
+    customer = Customer.create!(name: 'Chewie')
+    customer_two = Customer.create!(name: 'Anakin')
+    order.customer = customer
+    order.save!
+
+    patch order_path(order), params: { order: { customer_id: customer_two.id } }
+
+    assert_response :redirect
+    assert_equal order.reload.customer, customer_two
+    assert_equal customer_two.reload.orders.first, order
   end
 end
