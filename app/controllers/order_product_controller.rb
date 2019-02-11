c41dc77f46aaa98f8c4b8a2aa14c87425082c8da
brunats <silvatavares.bruna@gmail.com>
2019-02-11 17:49:40 -0200

    Edit order product

--- app/controllers/order_product_controller.rb
+++ app/controllers/order_product_controller.rb
@@ -1,4 +1,15 @@
 class OrderProductController < ApplicationController
+  def edit
+    @order_product = OrderProduct.find(params[:id])
+  end
+
+  def update
+    @order_product = OrderProduct.find(params[:id])
+    return redirect_to(edit_order_path(@order_product.order)) if @order_product.update(update_params)
+    flash[:warning] = @order_product.errors.to_a.first
+    render :edit
+  end
+
   def create
     flash[:warning] = order_error unless order_product.valid? && order_product.save
     redirect_to(edit_order_path(order))
@@ -10,6 +21,10 @@ class OrderProductController < ApplicationController
   end
 
   private
+
+  def update_params
+    params[:order_product].permit(:units, :unit_price, :product_id)
+  end
 
   def order_error
     order_product.errors.to_a.first
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root 'orders#index'
   resources :orders, only: %i[new create edit update destroy]
-  resources :order_product, only: %i[create destroy]
+  resources :order_product, only: %i[edit update create destroy]
 end
