d1f1b85d0a0e09cb4ddcbd2ae45b2e5c5bd1e0a0
brunats <silvatavares.bruna@gmail.com>
2019-02-12 21:28:03 -0200

    Refac some UI

--- app/validators/minimum_price_validator.rb
+++ app/validators/minimum_price_validator.rb
@@ -2,7 +2,7 @@ class MinimumPriceValidator < ActiveModel::Validator
   def validate(order_product)
     @order_product = order_product
     return if @order_product.product.blank?
-    if @order_product.unit_price < minimun_value
+    if @order_product.unit_price.to_f < minimun_value
       @order_product.errors[:base] << I18n.t('.errors.difference_price_error', minimun_value: minimun_value)
     end
   end
@@ -10,6 +10,6 @@ class MinimumPriceValidator < ActiveModel::Validator
   private
 
   def minimun_value
-    @order_product.product.price * 0.9
+    @order_product.product.price.to_f * 0.9
   end
 end
--- app/validators/multiple_unit_validator.rb
+++ app/validators/multiple_unit_validator.rb
@@ -2,7 +2,7 @@ class MultipleUnitValidator < ActiveModel::Validator
   def validate(order_product)
     @order_product = order_product
     return if @order_product.product.blank? || multiple_unit.zero?
-    unless (@order_product.units % multiple_unit).zero?
+    unless (@order_product.units.to_i % multiple_unit).zero?
       @order_product.errors[:base] << I18n.t('.errors.multiple_unit_error', multiple_value: multiple_unit)
     end
   end
@@ -10,6 +10,6 @@ class MultipleUnitValidator < ActiveModel::Validator
   private
 
   def multiple_unit
-    @order_product.product.multiple
+    @order_product.product.multiple.to_i
   end
 end
